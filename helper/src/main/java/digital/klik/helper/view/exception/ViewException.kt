ed5d831f16a411c48d2e70f4c24c6f9652016da5
Dika Wardani <dikawardani24@gmail.com>
2020-10-08 01:21:12 +0700

    add input extension

---
+++ helper/src/main/java/digital/klik/helper/view/exception/ViewException.kt
@@ -0,0 +1,17 @@
+package digital.klik.helper.view.exception
+
+import digital.klik.helper.exception.AppException
+
+@Suppress("unused")
+class ViewException : AppException {
+    constructor() : super()
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    constructor(cause: Throwable?) : super(cause)
+    constructor(
+        message: String?,
+        cause: Throwable?,
+        enableSuppression: Boolean,
+        writableStackTrace: Boolean
+    ) : super(message, cause, enableSuppression, writableStackTrace)
+}
---
+++ helper/src/main/java/digital/klik/helper/view/extension/TextInputLayoutExtensions.kt
@@ -0,0 +1,48 @@
+package digital.klik.helper.view.extension
+
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
+import digital.klik.helper.view.exception.ViewException
+
+val TextInputLayout.requiredEditText: EditText
+    get() = editText ?: throw ViewException("No EditText has been attached to current TextInputLayout")
+
+var TextInputLayout.text: String
+    set(value) = requiredEditText.setText(value)
+    get() = requiredEditText.text.toString()
+
+fun TextInputLayout.setText(value: String?, default: String = "") {
+    text = value ?: default
+}
+
+fun TextInputLayout.clear() {
+    requiredEditText.text = null
+}
+
+
+fun TextInputLayout.setErrorInput(error: CharSequence?, isUseEndIcon: Boolean = false) {
+    if (!isUseEndIcon) {
+        errorIconDrawable = null
+    }
+
+    setError(error)
+    if (error == null) clearError()
+}
+
+fun TextInputLayout.setErrorInput(errorResId: Int, isUseEndIcon: Boolean = false) {
+    if (!isUseEndIcon) {
+        errorIconDrawable = null
+    }
+
+    val message = context.getString(errorResId)
+    error = message
+}
+
+fun TextInputLayout.clearError() {
+    error = null
+    isErrorEnabled = false
+}
+
+fun TextInputLayout.addAfterTextChangedListener(afterTextChangedListner: (newText: String)-> Unit) {
+    requiredEditText.addAfterTextChangedListener(afterTextChangedListner)
+}
