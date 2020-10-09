7dc8ae11506448d7bed5e2751abf86bcee802b8b
Dika Wardani <dikawardani24@gmail.com>
2020-10-09 15:31:45 +0700

    change connection helper

--- helper/src/main/java/digital/klik/helper/network/ConnectivityHelper.kt
+++ helper/src/main/java/digital/klik/helper/network/ConnectivityHelper.kt
@@ -2,15 +2,14 @@ package digital.klik.helper.network
 
 import android.content.Context
 import android.net.ConnectivityManager
-import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("unused")
 object ConnectivityHelper {
 
     @Suppress("DEPRECATION")
-    fun verifyAvailableNetwork(activity: AppCompatActivity): Boolean {
+    fun verifyAvailableNetwork(context: Context): Boolean {
         val connectivityManager =
-            activity.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
         return networkInfo != null && networkInfo.isConnected
     }
