fed0992636e4d899b6bc6fefd4dfd0bc30a3904b
Dika Wardani <dikawardani24@gmail.com>
2020-10-09 15:20:01 +0700

    add api executor extension

---
+++ helper/src/main/java/digital/klik/helper/api/extension/ApiExecutorExtension.kt
@@ -0,0 +1,15 @@
+package digital.klik.helper.api.extension
+
+import androidx.lifecycle.ViewModel
+import digital.klik.helper.Result
+import digital.klik.helper.api.ApiExecutorHelper
+import io.reactivex.Single
+import io.reactivex.observers.DisposableSingleObserver
+
+fun <T> ViewModel.singleExecute(single: Single<T>,
+                                handler: (result: Result<T>) -> Unit): DisposableSingleObserver<T> {
+    return ApiExecutorHelper.singleExecute(
+        single = single,
+        handler = handler
+    )
+}
