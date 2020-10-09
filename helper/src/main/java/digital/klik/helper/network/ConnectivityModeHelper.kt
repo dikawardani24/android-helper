857c8055321850cee16bc770bb7441fc3eacce4a
Dika Wardani <dikawardani24@gmail.com>
2020-10-09 16:36:43 +0700

    add connectivity live data

--- helper/src/main/java/digital/klik/helper/network/ConnectivityModeHelper.kt
+++ helper/src/main/java/digital/klik/helper/network/ConnectivityModeHelper.kt
@@ -2,48 +2,28 @@ package digital.klik.helper.network
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import android.os.Build
+import digital.klik.helper.network.constant.ConnectivityMode
 
 @Suppress("unused")
 object ConnectivityModeHelper {
 
-    enum class ConnectivityMode {
-        NONE,
-        WIFI,
-        MOBILE,
-        ETHERNET,
-        BLUETOOTH
-    }
-
     private fun checkConnectivity(context: Context): ConnectivityMode {
-
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         cm.run {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                getNetworkCapabilities(activeNetwork)?.run {
-                    return when {
-                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> ConnectivityMode.WIFI
-                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> ConnectivityMode.MOBILE
-                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> ConnectivityMode.ETHERNET
-                        hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> ConnectivityMode.BLUETOOTH
-                        else -> ConnectivityMode.NONE
-                    }
-                }
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val networkCapabilities = getNetworkCapabilities(activeNetwork)
+                networkCapabilities?.run {
+                    ConnectivityMode.from(this)
+                } ?: ConnectivityMode.NONE
+
             } else {
                 @Suppress("DEPRECATION")
                 activeNetworkInfo?.run {
-                    return when (type) {
-                        ConnectivityManager.TYPE_WIFI -> ConnectivityMode.WIFI
-                        ConnectivityManager.TYPE_MOBILE -> ConnectivityMode.MOBILE
-                        ConnectivityManager.TYPE_ETHERNET -> ConnectivityMode.ETHERNET
-                        ConnectivityManager.TYPE_BLUETOOTH -> ConnectivityMode.BLUETOOTH
-                        else -> ConnectivityMode.NONE
-                    }
-                }
+                    ConnectivityMode.from(type)
+                } ?: ConnectivityMode.NONE
             }
         }
-        return ConnectivityMode.NONE
     }
 }
---
+++ helper/src/main/java/digital/klik/helper/network/liveData/ConnectionLiveData.kt
@@ -0,0 +1,23 @@
+package digital.klik.helper.network.liveData
+
+import android.content.Context
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import androidx.lifecycle.MutableLiveData
+import digital.klik.helper.network.broadcast.NetworkBroadcastReceiver
+import digital.klik.helper.network.model.ConnectionInfo
+
+class ConnectionLiveData(private val context: Context): MutableLiveData<ConnectionInfo>() {
+    private val networkReceiver = NetworkBroadcastReceiver(this)
+
+    override fun onActive() {
+        super.onActive()
+        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        context.registerReceiver(networkReceiver, filter)
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        context.unregisterReceiver(networkReceiver)
+    }
+}
