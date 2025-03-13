6908a3646d07ab2b778684bab6420feda8e2d164
dikawardaniTrainer <dikawardani.trainer@gmail.com>
2025-03-13 10:19:24 +0700

    add Permission Extension

--- ui-helper/src/main/java/dika/helper/android/navigation/NavigationExtension.kt
+++ ui-helper/src/main/java/dika/helper/android/navigation/NavigationExtension.kt
@@ -1,10 +1,11 @@
 @file:Suppress("unused")
 
-package dika.helper.android.activity.extension
+package dika.helper.android.navigation
 
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import androidx.fragment.app.Fragment
 import kotlin.reflect.KClass
 
 
@@ -21,7 +22,6 @@ fun Activity.setActivityResult(resultCode: Int, block: Intent.() -> Unit = {}) {
     finish()
 }
 
-
 fun Activity.setActivityResult(resultCode: Int, kClass: KClass<*>, block: Intent.() -> Unit = {}) {
     val intent = Intent(this, kClass.java)
     block(intent)
@@ -33,4 +33,12 @@ fun Context.startActivity(kClass: KClass<*>, block: Intent.() -> Unit = {}) {
     val intent = Intent(this, kClass.java)
     block(intent)
     startActivity(intent)
+}
+
+fun Fragment.startOtherActivityForResult(kClass: KClass<*>, requestCode: Int, block: Intent.() -> Unit = {}) {
+    requireActivity().startOtherActivityForResult(kClass, requestCode, block)
+}
+
+fun Fragment.startOtherActivity(kClass: KClass<*>, block: Intent.() -> Unit = {}) {
+    requireActivity().startActivity(kClass, block)
 }
---
+++ ui-helper/src/main/java/dika/helper/android/permission/PermissionExtension.kt
@@ -0,0 +1,70 @@
+@file:Suppress("unused")
+
+package dika.helper.android.permission
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import dika.helper.core.exception.IllegalArgsException
+import dika.helper.core.extension.logDebug
+
+fun Activity.checkHasPermission(permission: String): Boolean {
+    if (permission.isEmpty()) throw IllegalArgsException("Permission cannot be empty")
+    val result = checkSelfPermission(permission)
+    val granted = result == PackageManager.PERMISSION_GRANTED
+    logDebug("Permission : $permission, isGranted : $granted")
+    return granted
+}
+
+fun Activity.checkHasPermissions(permissions: Array<out String>): Boolean {
+    if (permissions.isEmpty()) throw IllegalArgsException("Permissions length must be greater than 0")
+    permissions.forEach { permission ->
+        if (!checkHasPermission(permission)) {
+            logDebug("Check permission for $permissions : false")
+            return false
+        }
+    }
+    logDebug("Check permission for $permissions : true")
+    return true
+}
+
+fun Activity.isPermissionGranted(
+    requiredPermission: String,
+    permissions: Array<out String>,
+    grantResults: IntArray
+): Boolean {
+    if (requiredPermission.isEmpty()) throw IllegalArgsException("Required permission cannot be empty")
+    if (permissions.isEmpty()) throw IllegalArgsException("Result permission cannot be empty")
+    if (grantResults.isEmpty()) throw IllegalArgsException("Grant results cannot be empty")
+    if (permissions.size != grantResults.size) throw IllegalArgsException("Permissions length must be equal to grant results length")
+
+    permissions.forEachIndexed { index, permission ->
+        if (permission == requiredPermission) {
+            val granted = grantResults[index] == PackageManager.PERMISSION_GRANTED
+            logDebug("Required permission: $requiredPermission, isGranted: $granted")
+            return granted
+        }
+    }
+    logDebug("Required permission: $requiredPermission, isGranted: false")
+    return false
+}
+
+fun Activity.isPermissionsGranted(
+    requiredPermissions: Array<out String>,
+    permissions: Array<out String>,
+    grantResults: IntArray
+): Boolean {
+    if (requiredPermissions.isEmpty()) throw IllegalArgsException("Required permission cannot be empty")
+    if (permissions.isEmpty()) throw IllegalArgsException("Result permission cannot be empty")
+    if (grantResults.isEmpty()) throw IllegalArgsException("Grant results cannot be empty")
+    if (permissions.size != grantResults.size) throw IllegalArgsException("Permissions length must be equal to grant results length")
+
+    requiredPermissions.forEach { requiredPermission ->
+        if (!isPermissionGranted(requiredPermission, permissions, grantResults)) {
+            logDebug("IsPermissions for $requiredPermissions : false")
+            return false
+        }
+    }
+
+    logDebug("IsPermissions for $requiredPermissions : true")
+    return true
+}
